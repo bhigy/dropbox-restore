f980cb82d4f62161af838e50cffff5c832e98972
Bertrand Higy <bertrand.higy@protomail.com>
2022-09-10 10:59:39 +0200

    upgrade to gist version from Arakade (https://gist.github.com/Arakade/50f47ab38d8ab0c3bb2ebf29d2795119)

--- restore.py
+++ restore.py
@@ -2,8 +2,8 @@
 import sys, os, dropbox, time
 from datetime import datetime
 
-APP_KEY = 'hacwza866qep9o6'   # INSERT APP_KEY HERE
-APP_SECRET = 'kgipko61g58n6uc'     # INSERT APP_SECRET HERE
+APP_KEY = 'API_KEY'   # INSERT APP_KEY HERE
+APP_SECRET = 'APP_SECRET'     # INSERT APP_SECRET HERE
 DELAY = 0.2 # delay between each file (try to stay under API rate limits)
 
 HELP_MESSAGE = \
@@ -19,7 +19,7 @@ enabled extended version history). Please specify a cutoff date within the past
 from the source code."""
 
 def authorize():
-    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
+    flow = dropbox.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
     authorize_url = flow.start()
     print('1. Go to: ' + authorize_url)
     print('2. Click "Allow" (you might have to log in first)')
@@ -29,8 +29,7 @@ def authorize():
     except NameError:
         pass
     code = input("Enter the authorization code here: ").strip()
-    access_token, user_id = flow.finish(code)
-    return access_token
+    return  flow.finish(code).access_token
 
 
 def login(token_save_path):
@@ -41,7 +40,7 @@ def login(token_save_path):
         access_token = authorize()
         with open(token_save_path, 'w') as token_file:
             token_file.write(access_token)
-    return dropbox.client.DropboxClient(access_token)
+    return dropbox.Dropbox(access_token)
 
 
 def parse_date(s):
@@ -50,11 +49,15 @@ def parse_date(s):
 
 
 def restore_file(client, path, cutoff_datetime, is_deleted, verbose=False):
-    revisions = client.revisions(path.encode('utf8'))
-    revision_dict = dict((parse_date(r['modified']), r) for r in revisions)
+    try:
+        revisions = client.files_list_revisions(path.encode('utf8'))
+    except:
+        print('no revisions on: ', path.encode('utf8'))
+        return
 
+    revision_dict = dict((r.server_modified, r) for r in revisions.entries)
     # skip if current revision is the same as it was at the cutoff
-    if max(revision_dict.keys()) < cutoff_datetime:
+    if len(revision_dict.keys()) > 0 and max(revision_dict.keys()) < cutoff_datetime:
         if verbose:
             print(path.encode('utf8') + ' SKIP')
         return
@@ -64,33 +67,42 @@ def restore_file(client, path, cutoff_datetime, is_deleted, verbose=False):
                            if d < cutoff_datetime]
     if len(pre_cutoff_modtimes) > 0:
         modtime = max(pre_cutoff_modtimes)
-        rev = revision_dict[modtime]['rev']
+        rev = revision_dict[modtime].rev
         if verbose:
             print(path.encode('utf8') + ' ' + str(modtime))
-        client.restore(path.encode('utf8'), rev)
+        client.files_restore(path.encode('utf8'), rev)
     else:   # there were no revisions before the cutoff, so delete
         if verbose:
             print(path.encode('utf8') + ' ' + ('SKIP' if is_deleted else 'DELETE'))
         if not is_deleted:
             client.file_delete(path.encode('utf8'))
+        # TODO: Restore functionality whereby a file that didn't exist at that
+        # point in time will get deleted.
+        #if not is_deleted:
+        #    client.file_delete(path.encode('utf8'))
 
 
-def restore_folder(client, path, cutoff_datetime, verbose=False):
+def restore_folder(client, path, cutoff_datetime, verbose=False, do_recurse=True):
     if verbose:
         print('Restoring folder: ' + path.encode('utf8'))
     try:
-        folder = client.metadata(path.encode('utf8'), list=True,
-                                 include_deleted=True)
-    except dropbox.rest.ErrorResponse as e:
+        folder = client.files_list_folder(path.encode('utf8'),
+                                          include_deleted=True)
+    except dropbox.exceptions.ApiError as e:
         print(str(e))
         print(HELP_MESSAGE)
         return
-    for item in folder.get('contents', []):
-        if item.get('is_dir', False):
-            restore_folder(client, item['path'], cutoff_datetime, verbose)
+    for item in folder.entries:
+        if isinstance(item, dropbox.files.FolderMetadata):
+            if do_recurse:
+                restore_folder(client, item.path_lower, cutoff_datetime, verbose)
+            else:
+                print('not recursing into ', item.path_lower)
         else:
-            restore_file(client, item['path'], cutoff_datetime,
-                         item.get('is_deleted', False), verbose)
+            # TODO: Restore functionality whereby a file that didn't exist at that
+            # point in time will get deleted.
+            restore_file(client, item.path_lower, cutoff_datetime,
+                         False, verbose)
         time.sleep(DELAY)
 
 
@@ -106,7 +118,7 @@ def main():
     if cutoff_datetime > datetime.utcnow():
         sys.exit('Cutoff date must be in the past')
     client = login('token.dat')
-    restore_folder(client, root_path, cutoff_datetime, verbose=True)
+    restore_folder(client, root_path, cutoff_datetime, verbose=True, do_recurse=False) # TEMP: disable recursion
 
 
 if __name__ == '__main__':
